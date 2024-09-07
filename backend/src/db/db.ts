0391096ff7fa4d36fd0f6971ec1cf9e480c8129f
Paula Jimenez <paulajimenezt20@gmail.com>
2024-09-07 02:52:03 -0500

    Fix dockerfile for monorepo

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,8 +1,11 @@
 import sqlite3 from "sqlite3";
+import path from "path";
 import TasksRepository from "./repositories/tasks.repository";
 
 // If we're not running on docker just use an in-memory database
-const DATABASE = process.env.DOCKER_DB_PATH ?? ":memory:";
+const DATABASE = process.env.DB_PATH
+  ? path.resolve(process.env.DB_PATH)
+  : ":memory:";
 
 const initializeRepositories = async () => {
   const db = new sqlite3.Database(DATABASE, sqlite3.OPEN_READWRITE, (err) => {
@@ -10,7 +13,6 @@ const initializeRepositories = async () => {
       console.error(err.message);
     }
   });
-  console.log(`Connected to the database`);
   const tasksRepository = new TasksRepository(db);
   await tasksRepository.createTable();
 
